a05220af6dcd84282a2a142600b4e4f91cc0b107
Likhith Chitneni <git@chittti.com>
2016-11-01 18:53:17 -0600

    Added all src files

---
+++ src/pilot.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016-present, Society for Artificial intelligence - BYUI.
+ * All rights reserved.
+ *
+ * This source code is licensed under license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <iostream>
+
+int pilot() {
+    //Pilot code goes here
+    return 0;
+}
